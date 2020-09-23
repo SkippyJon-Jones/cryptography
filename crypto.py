322b77e08255c1fe6805694c4d3d5685df33d2aa
Adam <adam.mhatre@menloschool.org>
2020-09-23 15:10:48 -0700

    bad publick key code

--- crypto.py
+++ crypto.py
@@ -1,11 +1,16 @@
-# Caesar Cipher
+import random
+import math
 # Arguments: string, integer
 # Returns: string
 def encrypt_caesar(plaintext, offset):
     encrypt = ""
+    
+
     for char in plaintext:
+        #checks if the character is a valid one to shift
         if(ord(char) > 64 and ord(char) < 91):
             changed = ord(char) + offset
+            #checks if the shift goes over the ascii value for z
             if(changed > 90):
                 changed = changed - 26
             encrypt = encrypt + chr(changed)
@@ -15,9 +20,12 @@ def encrypt_caesar(plaintext, offset):
 # Returns: string
 def decrypt_caesar(ciphertext, offset):
     decrypt = ""
+
     for char in ciphertext:
+        #checks if the character is a valid one to shift back
         if(ord(char) > 64 and ord(char) < 91):
             changed = ord(char) - offset
+            #checks if the shift goes under the ascii value for a
             if(changed < 65):
                 changed = changed + 26
             decrypt = decrypt + chr(changed)
@@ -57,7 +65,7 @@ def decrypt_vigenere(ciphertext, keyword):
     actualkeyword = ""
 
     i = 0
-    for char in ciphertext:
+    for char in Cipherertext:
         actualkeyword = actualkeyword + keyword[i]
         if(i == len(keyword) - 1): 
             i = -1
@@ -78,12 +86,35 @@ def decrypt_vigenere(ciphertext, keyword):
 # Arguments: integer
 # Returns: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 def generate_private_key(n=8):
-    pass
+    w = [1]
+    i = 1
+    total = 1
+    while i < n:
+        w.append(random.randint(total+1, 2*total))
+        total = total + w[i]
+        i = i + 1
+
+    W = tuple(w)
+
+    Q = random.randint(total+1, 2*total)
+
+    while 1 == 1:
+        R = random.randint(2, Q-1)
+        if math.gcd(R,Q) == 1:
+            break
+
+    return (W, Q, R)
 
 # Arguments: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 # Returns: tuple B - a length-n tuple of integers
 def create_public_key(private_key):
-    pass
+    b = []
+    for x in private_key[1]:
+        b.append(private_key[3] * x % private_key[2])
+
+    B = tuple(b)
+
+    return B
 
 # Arguments: string, tuple (W, Q, R)
 # Returns: list of integers
@@ -96,8 +127,8 @@ def decrypt_mhkc(ciphertext, private_key):
     pass
 
 def main():
-    print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
-    print(decrypt_vigenere("LXFOPVEFRNHR", "LEMON"))
+    print(generate_private_key())
+    print(create_public_key(generate_private_key()))
 
 if __name__ == "__main__":
     main()
